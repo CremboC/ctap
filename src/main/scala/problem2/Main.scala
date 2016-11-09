e026aba7682dd1fd5ff1e7248e8679ec56f3e21f
Paulius Imbrasas <cremboc@gmail.com>
2016-11-09 19:40:12 +0000

    Latest attempt diff analysis

--- src/main/scala/problem2/Main.scala
+++ src/main/scala/problem2/Main.scala
@@ -40,14 +40,6 @@ object Main {
     val permBox = new PermutationBox(permboxValues)
     val heys = new Heys(size, sbox, permBox)
 
-    //    val period = 0 until Math.pow(2, 4).toInt
-    //    val matchingInputs = for (
-    //      o1 <- period;
-    //      o2 <- period
-    //      if o1 < o2
-    //      if o1.toBinarySeq(4).binaryDifferenceTo(o2.toBinarySeq(4)).toInt == "1100".bin
-    //    ) yield (o1, o2)
-
     @inline
     def partialDecrypt(cipher: Int): Seq[Int] = {
       val o1bin = cipher.toBinarySeq(size)
@@ -62,18 +54,11 @@ object Main {
       substituted
     }
 
-    val dU = "0110000010000110".bin.toBinarySeq(size).grouped(4).map(_.toInt).toSeq
-
-    def compareToExpected(result: Seq[Int]): Boolean = {
-      result.head == dU.head && result(2) == dU(2) && result(3) == dU(3)
+    def compareToExpected(result: Seq[Int], expected: Seq[Int]): Boolean = {
+      result.head == expected.head && result(2) == expected(2) && result(3) == expected(3)
     }
 
-    //    println(matchingInputs.size)
-
-    println(java.lang.Long.parseLong("1100000000000000", 2))
-    println(Integer.parseInt("1100000000000000", 2))
-
-    val r = for (i <- 0 until 2 ** 12) yield s"1100${i.toBinarySeq(12).mkString("")}".bin
+    val dU = "0110000010000110".bin.toBinarySeq(size).grouped(4).map(_.toInt).toSeq
     val dP = "110000000000".bin
 
     val acc = mutable.Map[Int, Int]().par
@@ -89,88 +74,96 @@ object Main {
     })
 
     var plaintextTests = 0
-    val plaintextSize = r.size
 
-    r.par.foreach { p1 =>
+    (0 until 2 ** 16).par.foreach { p1 =>
       val p2 = p1 ^ dP
+      if (p2 < p1) {
+        for (key <- keys) {
+          val o1 = givenCiphers(p1)
+          val pdo1 = partialDecrypt(heys.subkeyMix(o1, key))
 
-      for (key <- keys) {
-        val o1 = givenCiphers(p1)
-        val pdo1 = partialDecrypt(heys.subkeyMix(o1, key))
+          val o2 = givenCiphers(p2)
+          val pdo2 = partialDecrypt(heys.subkeyMix(o2, key))
 
-        val o2 = givenCiphers(p2)
-        val pdo2 = partialDecrypt(heys.subkeyMix(o2, key))
-
-        val diff = pdo1.binaryDifferenceTo(pdo2) // 4. XOR
-        if (compareToExpected(diff)) {
-          acc.get(key) match {
-            case None => acc.put(key, 1)
-            case Some(v) =>
-              acc.put(key, v + 1)
-//              acc(key) = v + 1
+          val diff = pdo1.binaryDifferenceTo(pdo2) // 4. XOR
+          if (compareToExpected(diff, dU)) {
+            acc.get(key) match {
+              case None => acc.put(key, 1)
+              case Some(v) =>
+                acc.put(key, v + 1)
+            }
           }
         }
-      }
 
-      plaintextTests += 1
-      if (plaintextTests % 100 == 0) {
-        println(plaintextTests / plaintextSize.toDouble)
+        plaintextTests += 1
+        if (plaintextTests % 100 == 0) {
+          println(plaintextTests / ((2 ** 16).toDouble / 2))
+        }
       }
     }
 
-    println(acc)
+//    println(acc)
 
 
-    //    println(heys.substitute("1100000000000000".bin))
-    //    val validPairs2 = validPairs.take(validPairs.size / 2).filter {
-    //      case (o1, o2) =>
-    //        val o1bin = heys.permute(o1)
-    //        val o2bin = heys.permute(o2)
-    //        val p1 = s"00000000${o1bin}0000".bin
-    //        val p2 = s"00000000${o2bin}0000".bin
-    //
-    //        val perm1 = heys.substitute(o1bin)
-    //        val perm2 = heys.substitute(o2bin)
-    //
-    //        perm1.toBinarySeq(16).binaryDifferenceTo(perm2.toBinarySeq(16)).mkString("") == "0000000001100000"
-    //    }
-    //
-    //    println(validPairs2)
+//    for () yield
+
+//    val pp = for (
+//      i <- 0 until 2 ** 4;
+//      j <- 0 until 2 ** 4
+//    ) yield {
+//      s"${i.toBinarySeq(4).mkString("")}0000${i.toBinarySeq(4).mkString("")}0000".bin
+//    }
 
 
-    //    val p1 = "0000001000000000".bin
-    //    val p2 = "0000100100000000".bin
-    //
-    //    val o1 = heys.permute(heys.substitute(p1))
-    //    val o2 = heys.permute(heys.substitute(p2))
-    //
-    //
-    //
-    //    println(o1.toBinarySeq(16).mkString(""))
-    //    println(o2.toBinarySeq(16).mkString(""))
-    //    println(s"Diff: ${o1.toBinarySeq(16).binaryDifferenceTo(o2.toBinarySeq(16)).mkString("")}")
-    //    println(s"Expc: 0000001000000000")
-    //    val binarySeq = p1.toBinarySeq(size)
-    //    val result1 = sbox.substitute(binarySeq.view(0, 4).toInt)
-    //    val result2 = sbox.substitute(binarySeq.view(4, 8).toInt)
-    //    val result3 = sbox.substitute(binarySeq.view(8, 12).toInt)
-    //    val result4 = sbox.substitute(binarySeq.view(12, 16).toInt)
-    //
-    //    val output = result1.toBinarySeq(4) ++ result2.toBinarySeq(4) ++ result3.toBinarySeq(4) ++ result4.toBinarySeq(4)
-    //
-    //    println(output.mkString(""))
+//    val ddU = "0000100000000000".grouped(4).map(_.bin).toSeq
+////      .bin.toBinarySeq(size).grouped(4).map(_.toInt).toSeq
+//
+//    val ddP = "0000110100001101".bin
+//
+////    (0 until 2 ** 16).map(_ ^ ddP)
+////    val pp = (0 until 2 ** 16).collect { case x if (x ^ ddP) < x => x ^ ddP}
+//
+////    println(pp)
+//
+//    val keys2 = for (
+//      bits <- 0 until 2 ** 4
+//    ) yield ("0000" + bits.toBinarySeq(4).mkString("") + "00000000").bin
+//
+//    def compareToExpected2(result: Seq[Int], expected: Seq[Int]): Boolean = {
+//      result(1) == expected(1)
+//    }
+//
+//    (0 until 2 ** 16).par.foreach { p1 =>
+//      val p2 = p1 ^ dP
+//
+//      for (key <- keys2) {
+//        val o1 = givenCiphers(p1)
+//        val pdo1 = partialDecrypt(heys.subkeyMix(o1, key))
+//
+//        val o2 = givenCiphers(p2)
+//        val pdo2 = partialDecrypt(heys.subkeyMix(o2, key))
+//
+//        val diff = pdo1.binaryDifferenceTo(pdo2) // 4. XOR
+//        if (compareToExpected2(diff, ddU)) {
+//          acc.get(key) match {
+//            case None => acc.put(key, 1)
+//            case Some(v) =>
+//              acc.put(key, v + 1)
+//          }
+//        }
+//      }
+//
+//      plaintextTests += 1
+//      if (plaintextTests % 100 == 0) {
+//        println(plaintextTests.toDouble / (2 ** 16).toDouble)
+//      }
+//    }
+//
+//    println(acc)
 
+    acc.map { case (key, value) => (key, value.toDouble) }.toSeq.seq.sortBy(_._2).foreach { case (key, value) => println(s"$key -> " + "%f".format(value)) }
 
-    //    val out1 = heys.iteration(p1)
-    //    println(out1.toBinarySeq(size).mkString(""))
-
-    //    val out2 = heys.iteration(out1)
-    //    val out3 = heys.iteration(out2)
-    //    val out4 = heys.iteration(out3)
-    //    val out4permuted = permBox.permute(out4)
-    //    println(out4.toBinarySeq(size).mkString(""))
-
-
+//    val p = for (i <- 0 until 2 ** 12) yield s"0000${i.toBinarySeq(12).mkString("")}".bin
   }
 
 
