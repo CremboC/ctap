9805bef593c31bc644e6a9a8a2e1a481a0cba505
Paulius Imbrasas <cremboc@gmail.com>
2016-11-16 20:22:06 +0000

    Problem1 rewritten to use ints

---
+++ src/main/scala/package1int/Problem1Main.scala
@@ -0,0 +1,195 @@
+package package1int
+
+import package1int.Problem1Main.{R1, R2, R3}
+
+import scala.io.Source
+
+object Problem1Main {
+
+  import Problem1.ExtendedInt
+
+  // simplified combining function
+  @inline
+  def combineRegisterBits(a: Int, b: Int, c: Int): Int = 1 ^ a ^ b ^ (b & c)
+
+  case class Register(size: Int, taps: Seq[Int])
+
+  // 0th bit is the rightmost
+  // 10101
+  // ^   ^0th
+  // 5th
+  val R1 = Register(7, Seq(0, 1))
+  val R2 = Register(11, Seq(0, 2))
+  val R3 = Register(13, Seq(5, 2, 1, 0))
+
+  def main(args: Array[String]): Unit = {
+    Tests.test1()
+    Tests.test2()
+    Tests.test3()
+    Tests.test4()
+    Tests.challenge()
+
+    // read in the test stream in a nice format
+    val testStream = Source.fromFile("TestStream.txt").getLines().next().split(" ").map(_.toInt).toSeq
+    val iterations = 2000
+
+    /**
+      * Next block of code gets the LSFR1 key
+      */
+    val similarities = (1 to 2 ** R1.size).map { comb =>
+      val lsfr = new LSFR(comb, R1.taps, R1.size)
+      val output = for (_ <- 1 to iterations) yield lsfr.shift
+
+      (output.zip(testStream).count(x => x._1 == x._2) / iterations.toDouble, comb)
+    }
+    val r1result = similarities.find(s => s._1 < 0.30 || s._1 > 0.70).map(_._2).get
+
+    println(s"R1 key: $r1result (${r1result.paddedInt(R1.size)})")
+
+    /**
+      * Next block of code gets the LSFR2 key
+      */
+    // this will be used to get the LSFR2 key using the x1 XOR x2 method
+    val r1stream = {
+      val lsfr = new LSFR(r1result, R1.taps, R1.size)
+      for (_ <- 1 to iterations) yield lsfr.shift
+    }
+
+    val r1r2similarity = (1 to 2 ** R2.size).map { comb =>
+      val lsfr = new LSFR(comb, R2.taps, R2.size)
+      val output = for (_ <- 1 to iterations) yield lsfr.shift
+      val xorStream = for (i <- r1stream.indices) yield r1stream(i) ^ output(i)
+      (xorStream.zip(testStream).count(x => x._1 == x._2) / iterations.toDouble, comb)
+    }
+
+    val r2result = r1r2similarity.find(x => x._1 > 0.6 || x._1 < 0.4).map(_._2).get
+    println(s"R2 key: $r2result (${r2result.paddedInt(R2.size)})")
+
+    /**
+      * Next block of code gets the LSFR3 key
+      */
+    val r3result = (1 to 2 ** R3.size).find { comb =>
+      // for every combination we will create the three LSFRs in their corresponding state
+      val lsfr1 = new LSFR(r1result, R1.taps, R1.size)
+      val lsfr2 = new LSFR(r2result, R2.taps, R2.size)
+      val lsfr3 = new LSFR(comb, R3.taps, R3.size)
+
+      // and then shift for n amount of iterations, storing everything in a list
+      val outs = for (_ <- 1 to iterations) yield {
+        combineRegisterBits(lsfr1.shift, lsfr2.shift, lsfr3.shift)
+      }
+
+      // finally we simply do a element-wise comparison
+      outs == testStream
+    }
+
+    r3result match {
+      case Some(key) => println(s"R3 key: $key (${key.paddedInt(R3.size)})")
+      case None => println("R3 key not found.")
+    }
+
+  }
+}
+
+/**
+  * Source of bitwise operators: http://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit-in-c-c
+  *
+  * @param state initial state of the LSFR
+  * @param taps which bits to tap
+  * @param size number of bits in this LSFR
+  */
+class LSFR(var state: Int, val taps: Seq[Int], val size: Int) {
+  def shift: Int = {
+    val lastBit = state & 1
+
+    val bits = for (x <- taps) yield (state >> x) & 1
+    val leftMostBit = bits.reduce(_ ^ _)
+
+    // shift onces
+    state >>= 1
+    // and then set the left most bit to the new one
+    state ^= (-leftMostBit ^ state) & (1 << size - 1)
+
+    lastBit
+  }
+}
+
+object Tests {
+  def test1(): Unit = {
+    val lsfr = new LSFR(44, R1.taps, R1.size)
+    val output = for (_ <- 0 until 25) yield lsfr.shift
+    require(output == "0011010010111011100110010".split("").map(_.toInt).toList, {
+      s"""
+         |Failed test 1
+         |Expected 0011010010111011100110010
+         |Got      ${output.mkString("")}
+       """.stripMargin
+    })
+  }
+
+  def test2(): Unit = {
+    val lsfr = new LSFR(555, R2.taps, R2.size)
+    val output = for (_ <- 0 until 25) yield lsfr.shift
+    require(output == "1101010001010000101000100".split("").map(_.toInt).toList, {
+      s"""
+         |Failed test 2
+         |Expected 1101010001010000101000100
+         |Got      ${output.mkString("")}
+       """.stripMargin
+    })
+  }
+
+  def test3(): Unit = {
+    val lsfr = new LSFR(616, R3.taps, R3.size)
+    val outs = for (_ <- 0 until 25) yield lsfr.shift
+    require(outs == "0001011001000101010110111".split("").map(_.toInt).toList, {
+      s"""
+         |Failed test 3
+         |Expected 0001011001000101010110111
+         |Got      ${outs.mkString("")}
+       """.stripMargin
+    })
+  }
+
+  def test4(): Unit = {
+    val r0 = new LSFR(44, R1.taps, R1.size)
+    val r1 = new LSFR(555, R2.taps, R2.size)
+    val r2 = new LSFR(616, R3.taps, R3.size)
+
+    val outs = for (_ <- 0 until 25) yield {
+      Problem1Main.combineRegisterBits(r0.shift, r1.shift, r2.shift)
+    }
+
+    require(outs == "0000101101010100110001101".split("").map(_.toInt).toList, {
+      s"""
+         |Failed overall algorithm
+         |Expected 0000101101010100110001101
+         |Got      ${outs.mkString("")}
+       """.stripMargin
+    })
+  }
+
+  def challenge(): Unit = {
+    val r0 = new LSFR(97, R1.taps, R1.size)
+    val r1 = new LSFR(975, R2.taps, R2.size)
+    val r2 = new LSFR(6420, R3.taps, R3.size)
+
+    val outs = for (_ <- 0 until 25) yield {
+      Problem1Main.combineRegisterBits(r0.shift, r1.shift, r2.shift)
+    }
+
+    println(s"Challenge Vector output: ${outs.mkString("")}")
+  }
+}
+
+package object Problem1 {
+
+  implicit class ExtendedInt(i: Int) {
+    def **(pow: Int): Int = Math.pow(i, pow).toInt
+
+    def paddedInt(size: Int): String = {
+      String.format("%5s", i.toBinaryString).replace(' ', '0')
+    }
+  }
+
+}
